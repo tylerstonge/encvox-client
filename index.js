af995731d39495886399eb24ea4a64dcd52c410b
Tyler St. Onge <tylertstonge@gmail.com>
2017-07-11 03:04:29 -0400

    adhering to a coding standard

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
-const {app, ipcMain, BrowserWindow, Menu} = require('electron');
+'user strict';
+
+const { app, ipcMain, BrowserWindow } = require('electron');
 const path = require('path');
 const url = require('url');
 const fs = require('fs');
@@ -10,105 +12,100 @@ let win;
 let key;
 let registry = {};
 
-function loadOrCreateKey() {
-    key = new NodeRSA();
-    fs.readFile('./public.key', (err, pub) => {
-        if (err) {
-            // Generate key
-            key.generateKeyPair();
+function loadOrCreateKey () {
+  key = new NodeRSA();
+  fs.readFile('./public.key', (err, pub) => {
+    if (err) {
+      // Generate key
+      key.generateKeyPair();
 
-            // Save key
-            let publicPem = key.exportKey('public');
-            fs.writeFile('./public.key', publicPem, (err) => {
-                if (err) { console.log('could export public key'); }
-            });
-            let privatePem = key.exportKey('private');
-            fs.writeFile('./private.key', privatePem, (err) => {
-                if (err) { console.log('could export private key'); }
-            });
+      // Save key
+      let publicPem = key.exportKey('public');
+      fs.writeFile('./public.key', publicPem, (err) => {
+        if (err) { console.log('could export public key'); }
+      });
+      let privatePem = key.exportKey('private');
+      fs.writeFile('./private.key', privatePem, (err) => {
+        if (err) { console.log('could export private key'); }
+      });
+    } else {
+      key.importKey(pub, 'public');
+      fs.readFile('./private.key', (err, prv) => {
+        if (err) {
+          console.log('could not find private key, but found public key.');
         } else {
-            key.importKey(pub, 'public');
-            fs.readFile('./private.key', (err, prv) => {
-                if (err) {
-                    console.log('could not find private key, but found public key.');
-                } else {
-                    key.importKey(prv, 'private');
-                    socket.emit('identify', {
-                        username: 'dropkick',
-                        publicKey: key.exportKey('public')
-                    });
-                }
-            });
+          key.importKey(prv, 'private');
+          socket.emit('identify', {
+            username: 'dropkick',
+            publicKey: key.exportKey('public')
+          });
         }
-    });
+      });
+    }
+  });
 }
 
 function createWindow () {
-    // Create the browser window.
-    win = new BrowserWindow({width: 800, height: 600});
+  // Create the browser window.
+  win = new BrowserWindow({width: 800, height: 600});
 
-    loadOrCreateKey();
+  loadOrCreateKey();
 
-    // Load or create keys
-    win.webContents.on('loadKey', () => {
-        attemptLoadKey();
-    });
+  win.setMenu(null);
 
-    win.setMenu(null);
+  win.webContents.openDevTools();
 
-    win.webContents.openDevTools();
+  // Message from view -- encrypt and send to server
+  ipcMain.on('encrypt', (event, message) => {
+    for (var e in registry) {
+      if (registry.hasOwnProperty(e)) {
+        let user = registry[e];
+        let userKey = new NodeRSA();
+        userKey.importKey(user.publicKey, 'public');
+        let encrypted = userKey.encrypt(message, 'base64');
+        socket.emit('message', {
+          recipient: user.id,
+          message: encrypted
+        });
+      }
+    }
+  });
 
-    // Message from view -- encrypt and send to server
-    ipcMain.on('encrypt', (event, message) => {
-        for (var e in registry) {
-            if (registry.hasOwnProperty(e)) {
-                let user = registry[e];
-                let userKey = new NodeRSA();
-                userKey.importKey(user.publicKey, 'public');
-                let encrypted = userKey.encrypt(message, 'base64');
-                socket.emit('message', {
-                    recipient: user.id,
-                    message: encrypted
-                });
-            }
-        };
-    });
+  // Initialize the user registry
+  socket.on('current-users', (users) => {
+    registry = users;
+  });
 
-    // Initialize the user registry
-    socket.on('current-users', (users) => {
-        registry = users;
-    });
+  // Message from server -- decrypt and pass to view
+  socket.on('message', (message) => {
+    let decrypted = key.decrypt(message, 'utf8');
+    win.webContents.send('message', decrypted);
+  });
 
-    // Message from server -- decrypt and pass to view
-    socket.on('message', (message) => {
-        let decrypted = key.decrypt(message, 'utf8');
-        win.webContents.send('message', decrypted);
-    });
+  // New user joined the server
+  socket.on('user-join', (user) => {
+    registry[user.id] = user;
+  });
 
-    // New user joined the server
-    socket.on('user-join', (user) => {
-        registry[user.id] = user;
-    });
+  // User disconnected from server
+  socket.on('user-leave', (id) => {
+    delete registry[id];
+  });
 
-    // User disconnected from server
-    socket.on('user-leave', (id) => {
-        delete registry[id];
-    })
+  // and load the index.html of the app.
+  win.loadURL(url.format({
+    pathname: path.join(__dirname, 'public/index.html'),
+    protocol: 'file:',
+    slashes: true
+  }));
 
-    // and load the index.html of the app.
-    win.loadURL(url.format({
-        pathname: path.join(__dirname, 'public/index.html'),
-        protocol: 'file:',
-        slashes: true
-    }));
-
-    // Emitted when the window is closed.
-    win.on('closed', () => {
-        // Dereference the window object, usually you would store windows
-        // in an array if your app supports multi windows, this is the time
-        // when you should delete the corresponding element.
-        win = null;
-    });
+  // Emitted when the window is closed.
+  win.on('closed', () => {
+    // Dereference the window object, usually you would store windows
+    // in an array if your app supports multi windows, this is the time
+    // when you should delete the corresponding element.
+    win = null;
+  });
 }
 
 // This method will be called when Electron has finished
@@ -118,17 +115,17 @@ app.on('ready', createWindow);
 
 // Quit when all windows are closed.
 app.on('window-all-closed', () => {
-    // On macOS it is common for applications and their menu bar
-    // to stay active until the user quits explicitly with Cmd + Q
-    if (process.platform !== 'darwin') {
-        app.quit();
-    }
+  // On macOS it is common for applications and their menu bar
+  // to stay active until the user quits explicitly with Cmd + Q
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
 });
 
 app.on('activate', () => {
-    // On macOS it's common to re-create a window in the app when the
-    // dock icon is clicked and there are no other windows open.
-    if (win === null) {
-        createWindow();
-    }
+  // On macOS it's common to re-create a window in the app when the
+  // dock icon is clicked and there are no other windows open.
+  if (win === null) {
+    createWindow();
+  }
 });
--- public/js/client.js
+++ public/js/client.js
@@ -1,24 +1,24 @@
 $(function () {
-    const contents = require('electron').ipcRenderer;
+  const contents = require('electron').ipcRenderer;
 
-    $('#msg').keydown((e) => {
-        var keypressed = event.keyCode || event.which;
-        if (keypressed == 13) {
-            contents.send('encrypt', $('#msg').val());
-            $('#msg').val('');
-            return false;
-        }
-    });
+  $('#msg').keydown((e) => {
+    var keypressed = e.keyCode || e.which;
+    if (keypressed === 13) {
+      contents.send('encrypt', $('#msg').val());
+      $('#msg').val('');
+      return false;
+    }
+  });
 
-    $('#btn').click(() => {
-        contents.send('encrypt', $('#msg').val());
-        $('#msg').val('');
-        return false;
-    })
+  $('#btn').click(() => {
+    contents.send('encrypt', $('#msg').val());
+    $('#msg').val('');
+    return false;
+  });
 
-    contents.on('message', function(event, msg) {
-        console.log('recv: ' + JSON.stringify(msg));
-        $('#messages').append($('<p>').text(msg));
-        $('.container').scrollTop($('#messages')[0].scrollHeight);
-    });
+  contents.on('message', (event, msg) => {
+    console.log('recv: ' + JSON.stringify(msg));
+    $('#messages').append($('<p>').text(msg));
+    $('.container').scrollTop($('#messages')[0].scrollHeight);
+  });
 });
